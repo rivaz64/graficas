367c5d485c73fa831b1f56c36945a0d3c572beb4
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2021-03-03 13:49:31 -0600

    qwerty

--- Graficos1/Graficos1/GraphicModule/src/mesh.cpp
+++ Graficos1/Graficos1/GraphicModule/src/mesh.cpp
@@ -1,21 +1,23 @@
 #include "mesh.h"
+namespace GraphicsModule {
+    short* mesh::getindices()
+    {
+        return indices;
+    }
 
-short* mesh::getindices()
-{
-    return indices;
-}
+    void mesh::setindices(std::initializer_list<short> i)
+    {
+        indices = (short*)i.begin();
+    }
 
-void mesh::setindices(std::initializer_list<short> i)
-{
-    indices = (short*)i.begin(); 
-}
+    mesh::vertex* mesh::getvertex()
+    {
+        return points;
+    }
 
-mesh::vertex* mesh::getvertex()
-{
-    return points;
-}
+    void mesh::setvertex(std::initializer_list<vertex> i)
+    {
+        points = (vertex*)i.begin();
+    }
 
-void mesh::setvertex(std::initializer_list<vertex> i)
-{
-    points = (vertex*)i.begin();
 }
