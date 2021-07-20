cce869504fec12b3f2e3341e6beb9b3d2656d0c3
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2021-07-20 13:49:49 -0500

    Update effecto.cpp

--- Graficos1/Graficos1/GraphicModule/src/effecto.cpp
+++ Graficos1/Graficos1/GraphicModule/src/effecto.cpp
@@ -4,9 +4,6 @@ void GraphicsModule::effecto::render()
 {
 	//pases[0]->render(&v);
 	for (int i = 0; i < pases.size(); i++){
-		if(i== pases.size()-1)
-		pases[i]->render(objts[i],false);
-		else
-		pases[i]->render(objts[i], pases[i+1]->clear);
+		pases[i]->render(objts[i],true);
 	}
 }
